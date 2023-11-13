6bc4c9a3a766432b63d944abaa30dcbb2654cc51
Artificialvira <arivarasana.aids2022@citchennai.net>
2023-11-13 17:31:00 +0530

    First Commit

---
+++ Hospital.Web/Hospital.Utilities/DbInitializer.cs
@@ -0,0 +1,51 @@
+﻿using Hospital.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Utilities
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private ApplicationDbContext _context;
+
+        public DbInitializer(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            this._context = context;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                if (_context.Database.GetPendingMigrations().Count() > 0)
+                {
+
+                }
+            }   
+            catch { }
+            if (!_roleManager.RoleExistsAsync(WebsiteRoles.Website_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(WebsiteRoles.Website_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WebsiteRoles.Website_Patient)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WebsiteRoles.Website_Doctor)).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "Nasavira",
+                    Email = "nasara@xyz.com"
+                },"nasara@123").GetAwaiter().GetResult();
+
+
+                var Appuser = _context.ApplicationUsers?.FirstOrDefault(x => x.Email == "nasara@xyz.com");
+                if (Appuser != null)
+                {
+                    _userManager.AddToRoleAsync(Appuser,WebsiteRoles.Website_Admin).GetAwaiter().GetResult();
+                }
+            }
+        }
+    }
+}
---
+++ Hospital.Web/Hospital.Utilities/WebsiteRoles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Utilities
+{
+    public static class WebsiteRoles
+    {
+        public const string Website_Admin = "Admin";
+        public const string Website_Doctor = "Doctor";
+        public const string Website_Patient = "Patient";
+
+
+    }
+}
